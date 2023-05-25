e93cdc37ddaf8ee9199ac40bee639429ee4df43e
siwon <ksw3721@gmail.com>
2023-05-25 17:40:44 +0900

    Upbit 클래스 Axios 세팅
    - Upbit 클래스 정의
    - startProcess 함수 정의 및 연결

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import path from "path";
+import startProcess from "./process";
 
 // 환경변수 세팅 - 환경변수를 사용하는 모듈은 아래에서 import
 dotenv.config();
+
+const access_key = process.env.UPBIT_OPEN_API_ACCESS_KEY as string;
+const secret_key = process.env.UPBIT_OPEN_API_SECRET_KEY as string;
+const server_url = process.env.UPBIT_OPEN_API_SERVER_URL as string;
 
 // 앱 시작
 const app = express();
@@ -32,5 +36,8 @@ app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
     process.env.NODE_ENV === "production" ? null : err?.toString();
   res.status(500).json({ message });
 });
+
+// process 실행
+startProcess(access_key, secret_key, server_url);
 
 export default app;
